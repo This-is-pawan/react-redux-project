6964987da8e76e239c4f5cf2415b256958919828
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-03-15 12:31:53 +0530

    Update Fetching.jsx

--- src/myapp/Fetching.jsx
+++ src/myapp/Fetching.jsx
@@ -58,7 +58,7 @@ const ProductList = () => {
                   ${product.price}
                 </span>
                 <button
-                  className="btn btn-primary"
+                  className="btn btn-primary w-[50%]"
                   onClick={() => dispatch(addToCart({ id: nanoid(), name: product.title, price:product.price,image:product.image }))}
                 >
                   Add to Cart
